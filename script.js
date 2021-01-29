7239ef3d2afd52dbac4881a2065ca0594c009924
rek9 <rekcoding@gmail.com>
2021-01-29 13:51:18 -0600

    Added Background Image, Restaurant, and additional comments. Removed Slider for now and made it a static selection. Still needs tightening up on styling...

--- script.js
+++ script.js
@@ -1,42 +1,107 @@
-//Get Bill Amount from input
-//Store Service Rating in variable
-//Get # of People sharing bill
-//Calculate tip based on Bill Amount and # of People
-
 let userBill;
 let rating;
-let people;
+let comment;
+
+//Get Bill Amount from input
 
 function getBill() {
     userBill = document.getElementById('amount').value;
     console.log(userBill);
 }
 
+
+//Store Service Rating in variable
+
 function getRating() {
     rating = document.getElementById('rating').value;
     console.log(rating);
 }
 
-function getPeople() {
-    people = document.getElementById('people').value
-    console.log(people);
+
+// Get additional comments
+
+function getComment() {
+    comment = document.getElementById('comment').value
+    console.log(comment);
 }
 
-var slider = document.getElementById("range");
+
+// Slider Functionality (Disabled) //
+
+/* var slider = document.getElementById("range");
   var output = document.getElementById("value");
   var outputVar = document.getElementById("outputVar");
 
   let update = () => output.innerHTML = slider.value;
 
   slider.addEventListener('input', update);
-  update();
+  update(); */
 
+
+//Calculate tip based on Bill Amount and inject into modal.
 
 function calculate() {
     getBill();
     getRating();
-    getPeople();
-    let tip = (output.innerHTML / 100) * Number(userBill);
-    tip = tip.toFixed(2);
-    alert("$" + tip + " tip!");
+    getComment();
+    
+    if (rating == 'Great') {
+        let tip = 0.2 * Number(userBill);
+        tip = tip.toFixed(2);
+        let tipInject = document.getElementById('modal-body');
+        tipInject.innerHTML = tip;    
+    } else if (rating == 'Okay') {
+        let tip = 0.15 * Number(userBill);
+        tip = tip.toFixed(2);
+        let tipInject = document.getElementById('modal-body');
+        tipInject.innerHTML = tip;
+    } else if (rating == 'Poor') {
+        let tip = 0.05 * Number(userBill);
+        tip = tip.toFixed(2);
+        let tipInject = document.getElementById('modal-body');
+        tipInject.innerHTML = tip;
+    }
+}
+
+
+
+// Modal Functionality. WIP.
+
+const openModalButtons = document.querySelectorAll('[data-modal-target]');
+const closeModalButtons = document.querySelectorAll('[data-close-button]');
+const overlay = document.getElementById('overlay');
+
+openModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = document.querySelector(button.dataset.modalTarget);
+        openModal(modal);
+    })
+})
+
+overlay.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.modal.active');
+    modals.forEach(modal => {
+        closeModal(modal);
+    })
+})
+
+closeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = button.closest('.modal');
+        closeModal(modal);
+    })
+})
+
+
+   
+function openModal(modal) {
+    if (modal == null) return;
+    modal.classList.add('active');
+    overlay.classList.add('active');
+}
+
+function closeModal(modal) {
+    if (modal == null) return;
+    modal.classList.remove('active');
+    overlay.classList.remove('active');
 }
